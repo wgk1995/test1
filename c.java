ae68b6f70bf432333c07d7ee98221b71bc47514b
jason <2858921629@qq.com>
2018-05-19 02:29:25 -0700

    99

--- c.java
+++ c.java
@@ -1,7 +1,7 @@
 public class c{
 	public static void main(String[] args){
-	for(int i=1,i<=9,i++){
-		for(j=1,j<=1,j++){
+	for(int i=1;i<=9;i++){
+		for(j=1,j<=i,j++){
 		System.out.print(j+"x"+i+"="+j*i);
 }
 	}
